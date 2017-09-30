c758ae30847bef1c42f737bf86eecfbd3376b9f4
s-gubernator <svitlana.gubernator@yahoo.com>
2017-09-30 23:27:33 +0300

    Finished user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  resources :users
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users 
 
   get  'pages/:page', to: 'pages#show', as: 'page'
-  get  '/signup', to: 'users#new'  
+  get  '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'  
   root 'pages#home'
 end
