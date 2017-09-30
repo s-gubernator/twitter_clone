848fb449b06c38d5b94c1293158817f630c351e7
s-gubernator <svitlana.gubernator@yahoo.com>
2017-09-30 17:51:33 +0300

    Added User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  resources :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get  'pages/:page', to: 'pages#show', as: 'page'   
+  get  'pages/:page', to: 'pages#show', as: 'page'
+  get  '/signup', to: 'users#new'  
   root 'pages#home'
 end
