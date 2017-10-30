9fb5d02a87b34000ac4e40bfd3b626ce1ed23a3b
s-gubernator <svitlana.gubernator@yahoo.com>
2017-10-30 17:43:53 +0200

    Implemented basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,7 @@ class UsersController < ApplicationController
   def create  	
     @user = User.new(user_params)
     if @user.save
+      log_in @user      
       flash[:success] = "Welcome to ToDo App!"      
       redirect_to @user
     else
