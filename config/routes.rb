6e4c16059bd93b7aa478a70ce3e8e35caf61126f
s-gubernator <svitlana.gubernator@yahoo.com>
2017-12-13 23:04:42 +0200

    User mailer - account activation (development)

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do  
 	root   'pages#home'
+
   get    'pages/:page', to: 'pages#show', as: 'page'
-  get    '/signup',     to: 'users#new'
-  # post   '/signup',     to: 'users#create'
+  get    '/signup',     to: 'users#new'  
   get    '/login',      to: 'sessions#new'
   post   '/login',      to: 'sessions#create'
   delete '/logout',     to: 'sessions#destroy'
+
   resources :users
+  resources :account_activations, only: [:edit]
 end
