d72f534628ebb6d7699fd92a3d3641646d842722
s-gubernator <svitlana.gubernator@yahoo.com>
2018-01-22 22:38:15 +0200

    Show user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,8 @@ class UsersController < ApplicationController
   end
 
   def show
-  	@user = User.find(params[:id])    
+  	@user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page], per_page: 10)    
     redirect_to root_url and return unless @user.activated?
   end
   
