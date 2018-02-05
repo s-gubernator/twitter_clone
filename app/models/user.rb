311557eeadb1c8ec3e6a316a11eb6438a3d88697
s-gubernator <svitlana.gubernator@yahoo.com>
2018-02-05 20:38:35 +0200

    Added destroying microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -67,7 +67,12 @@ class User < ApplicationRecord
   # Returns true if a password reset has expired
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
-  end  
+  end 
+
+  # Defines a proto-feed 
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
 
   private
     # Converts email to all lower-case
