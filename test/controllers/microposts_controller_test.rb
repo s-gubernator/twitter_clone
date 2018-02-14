7b5618af9058b7ea688fae91f533c9e4f70b8a9c
s-gubernator <svitlana.gubernator@yahoo.com>
2018-02-14 17:24:09 +0200

    Completed user microposts

--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -19,4 +19,14 @@ class MicropostsControllerTest < ActionDispatch::IntegrationTest
     end
     assert_redirected_to login_url
   end
+
+  test "should redirect destroy for wrong micropost" do
+    user = users(:david)
+    micropost = microposts(:ants)
+    log_in_as(user)
+    assert_no_difference 'Micropost.count' do
+      delete micropost_path(micropost)
+    end
+    assert_redirected_to root_url
+  end
 end
